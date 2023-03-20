2fd793eb514a66f586204e765d37fd3796d28a3b
Gagan7259 <109654682+Gagan7259@users.noreply.github.com>
2023-03-20 19:21:40 +0530

    java

---
+++ JavaFull/src/AccesModifies/Final.java
@@ -0,0 +1,13 @@
+package AccesModifies;
+
+final class Final {
+	 void m1() {
+		System.out.println("Hey good morning");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//m1();
+	}
+
+}
